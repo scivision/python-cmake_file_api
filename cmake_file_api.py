9a8b14113ea209d31d6ebee07f854d104e975a95
scivision <scivision@users.noreply.github.com>
2023-03-03 10:49:35 -0500

    add codemodel

--- cmake_file_api.py
+++ cmake_file_api.py
@@ -1,32 +1,46 @@
-from __future__ import annotations
+"""
+Complete standalone example of querying CMake file-api
+* cache
+* codemodel (targets, files, configurations)
 
+https://cmake.org/cmake/help/latest/manual/cmake-file-api.7.html
+"""
+
+from __future__ import annotations
 from pathlib import Path
 from dataclasses import dataclass
 import shutil
 import subprocess
-import logging
 import argparse
 import json
 from pprint import pprint
 
 
 @dataclass
-class Cmake():
+class Cmake:
 
     cmake_exe: str
     source_dir: Path
     build_dir: Path
+    api_dir: Path
+    query_dir: Path
+    resp_dir: Path
 
     def __init__(self, source_dir: str | None = None, build_dir: str | None = None):
 
         if cmake_exe := shutil.which("cmake"):
             self.cmake_exe = cmake_exe
         else:
-            raise FileNotFoundError('CMake executable not found')
+            raise FileNotFoundError("CMake executable not found")
 
         self.source_dir = Path(source_dir).expanduser().resolve() if source_dir else Path.cwd()
 
-        self.build_dir = Path(build_dir).expanduser().resolve() if build_dir else self.source_dir / 'build'
+        self.build_dir = Path(build_dir).expanduser().resolve() if build_dir else self.source_dir / "build"
+
+        self.api_dir = self.build_dir / ".cmake/api/v1"
+        self.query_dir = self.api_dir / "query"
+        self.query_dir.mkdir(parents=True, exist_ok=True)
+        self.resp_dir = self.api_dir / "reply"
 
     def generate(self) -> None:
         """
@@ -37,46 +51,61 @@ class Cmake():
 
         gen_cmd = [self.cmake_exe] + wopts
 
-        gen_cmd += [f"-S{self.source_dir}",  f"-B{self.build_dir}"]
+        gen_cmd += [f"-S{self.source_dir}", f"-B{self.build_dir}"]
         subprocess.check_call(gen_cmd)
 
+    def get_index(self) -> dict[str, str]:
+        """get index of CMake file-api reply files"""
+
+        self.generate()
+        indices = sorted(self.resp_dir.glob("index-*.json"))
+
+        return json.loads(indices[-1].read_text())
 
     def get_cache(self) -> dict[str, str]:
-        api_dir = self.build_dir / '.cmake/api/v1'
-        query_dir = api_dir / 'query'
-        query_dir.mkdir(parents=True, exist_ok=True)
+        """get CMake cache as dict"""
 
-        # request CMake Cache info
-        (query_dir / 'cache-v2').touch()
+        (self.query_dir / "cache-v2").touch()
 
-        resp_dir = api_dir / 'reply'
-        indices = sorted(resp_dir.glob('index-*.json'), reverse=True)
-        if indices:
-            index_fn = indices[0]
-            if (self.source_dir / 'CMakeLists.txt').stat().st_mtime > index_fn.stat().st_mtime:
-                logging.info('CMakeLists.txt modified after response index, regenerating')
-                self.generate()
-                index_fn = sorted(resp_dir.glob('index-*.json'), reverse=True)[0]
-        else:
-            logging.info('CMake run for first generation')
-            self.generate()
-            index_fn = sorted(resp_dir.glob('index-*.json'), reverse=True)[0]
+        index = self.get_index()
+        cache_fn = self.resp_dir / index["reply"]["cache-v2"]["jsonFile"]
 
-        index = json.loads(index_fn.read_text())
-        cache_fn = resp_dir / index['reply']['cache-v2']['jsonFile']
+        return json.loads(cache_fn.read_text())
 
-        cmakecache = json.loads(cache_fn.read_text())
-        cache = {entry['name']: entry['value'] for entry in cmakecache['entries']}
+    def get_codemodel(self) -> dict[str, str]:
+        """get CMake codemodel as dict"""
 
-        return cache
+        (self.query_dir / "codemodel-v2").touch()
+
+        index = self.get_index()
+        codemodel_fn = self.resp_dir / index["reply"]["codemodel-v2"]["jsonFile"]
+
+        codemodel = json.loads(codemodel_fn.read_text())
+
+        return codemodel
+
+    def get_target_jsonFiles(self) -> list[str]:
+        """get list of target JSON index files"""
+
+        codemodel = self.get_codemodel()
+        targets = [target["jsonFile"] for target in codemodel["configurations"][0]["targets"]]
+
+        return targets
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument('source_dir', help="Top level source directory of CMake project")
+    p.add_argument("source_dir", help="Top level source directory of CMake project")
     P = p.parse_args()
 
     cmake = Cmake(P.source_dir)
-    cache = cmake.get_cache()
 
-    pprint(cache)
+    # cache = cmake.get_cache()
+    # pprint(cache)
+
+    # codemodel = cmake.get_codemodel()
+    # pprint(codemodel)
+
+    for file in cmake.get_target_jsonFiles():
+        target = json.loads((cmake.resp_dir / file).read_text())
+        pprint(target)
